0a4d5460fcbbc3acbd1ad183b12e932d8a5a2f22
Joannis orlandos <joannis@orlandos.nl>
2021-06-08 11:41:36 +0200

    Remove async helper

--- Sources/CypherMessaging/AsyncAwait.swift
+++
@@ -1,8 +0,0 @@
-import _NIOConcurrency
-
-prefix operator <~
-
-@available(macOS 12, iOS 15, *)
-public prefix func <~<T>(value: EventLoopFuture<T>) async throws -> T {
-    try await value.get()
-}
